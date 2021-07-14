14baeed1036b9078456bb458486c2e187de0be6e
Apurv Purohit <42898081+ApurvPurohit@users.noreply.github.com>
2021-07-14 16:42:26 +0530

    Update configs for Sybase and Oracle

--- venv/Config.py
+++ venv/Config.py
@@ -5,6 +5,20 @@ Dev = {
 	"driver": "SQL Server",
     "port": "1433"
 }
+Oracle = {
+    "db_server": "es20-scan01",
+    "port": "1521",
+    "user": "<db_username>",
+    "passwd": "<password>",
+    "service_name": "cmc1st01svc.uhc.com"
+}
+Sybase = {
+    "db_server": "DBSPS0181",
+    "user": "<db_username>",
+    "passwd": "<password>",
+    "driver": "Adaptive Server Enterprise",
+    "port": "4106"
+}
 # Test = {
 #     "db_server": "",
 #     "user": "",
